a61d504dd4953b677c7af6ec95c88e81842dcf8b
queteeco1 <ferentst1@gmail.com>
2023-05-04 12:42:57 +0300

    start new project

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:untitled/services/data_services.dart';
+
+import 'cubit/app_cubit_logics.dart';
+import 'cubit/app_cubits.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider<AppCubits>(
+        create: (context) {
+          return AppCubits(
+            data: DataServices(),
+          );
+        },
+        child: const AppCubitLogics(),
+      ),
+      // routes: {
+      //   '/': (context) => const DetailPage(),
+      //   '/main_screen': (context) => const WelcomePage(),
+      // },
+      // initialRoute: '/',
+    ),
+  );
+}
